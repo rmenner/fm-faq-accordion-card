1723bf9293eb27873cccbe68bddfc00f8c7c006b
Ryan Menner <rwmenner@gmail.com>
2022-01-15 12:05:14 -0500

    feat: basic faq functions

---
+++ src/components/qa-item.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { slide } from 'svelte/transition';
+  export let item = {};
+  export let i = 0;
+  let active = i === 0;
+</script>
+<div class="pt-6">
+  <dt class="text-lg">
+    <!-- Expand/collapse question button -->
+    <button type="button" on:click="{() => active = !active}" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
+      <span class="font-medium text-gray-900">
+        {item.question}
+      </span>
+      <span class="ml-6 h-7 flex items-center">
+        <svg class="h-6 w-6 transform... {active ? 'rotate-0' : '-rotate-180'}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+        </svg>
+      </span>
+    </button>
+  </dt>
+  {#if active }
+  <dd transition:slide class="mt-2 pr-12" id="faq-0">
+    <p class="text-base text-gray-500">
+      {item.answer}
+    </p>
+  </dd>
+  {/if}
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script>
+  import faq from "../data/faq.json";
+  import Qa from "../components/qa-item.svelte";
+</script>
+
+
+<div class="bg-gray-50">
+  <div class="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+    <div class="max-w-3xl mx-auto divide-y-2 divide-gray-200">
+      <h2 class="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">
+        Frequently asked questions
+      </h2>
+      <dl class="mt-6 space-y-6 divide-y divide-gray-200">
+        {#each faq as item, i}
+          <Qa { item } { i }/>
+        {/each}
+      </dl>
+    </div>
+  </div>
+</div>
+
+  
+
+
+<div class="attribution">
+  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+  Coded by <a href="#">Your Name Here</a>.
+</div>
