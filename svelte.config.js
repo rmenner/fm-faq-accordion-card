2cd5ab9cf894cbebf9e31f3baf605aaffdb8f604
Ryan Menner <rwmenner@gmail.com>
2022-01-17 10:40:00 -0500

    feat: github pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapterGhpages from "svelte-adapter-ghpages";
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapterGhpages({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+		}),
+
+		paths: {
+			base: dev ? '' : '/fm-faq-accordion-card',
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
