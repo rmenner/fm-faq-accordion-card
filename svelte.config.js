06564a847fce7e61bcebb4961dd51b904c838746
Ryan Menner <rwmenner@gmail.com>
2022-01-17 10:48:22 -0500

    edit: change delployemnt to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,14 @@
 import preprocess from 'svelte-preprocess';
-import adapterGhpages from "svelte-adapter-ghpages";
-
-const dev = process.env.NODE_ENV === 'development';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapterGhpages({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
 		}),
-
-		paths: {
-			base: dev ? '' : '/fm-faq-accordion-card',
-		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
